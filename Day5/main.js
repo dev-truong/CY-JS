bbfbba25f09814dcd1d6d2b5773f9687e6991a04
TruongDQ <daoquangtruong151097@gmail.com>
2024-10-18 16:01:46 +0700

    day5 uncomplete

---
+++ Day5/main.js
@@ -0,0 +1,38 @@
+const dropArea = document.querySelector('.drop-area');
+const fileInput = document.getElementById('input');
+
+fileInput.addEventListener('change', (e) => {
+    document.querySelector('.drop-text').style.display = 'none';
+    const files = e.target.files;
+    for(let file of files) {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.addEventListener('load', (e) => {
+            const url = e.target.result;
+            const img = document.createElement('img');
+            img.src = url;
+            img.style.height = '80px';
+            img.style.width = '80px';
+            dropArea.appendChild(img);
+        })
+    }
+})
+
+dropArea.addEventListener("drop", (event) => {
+    event.preventDefault();
+    const files = event.dataTransfer.files;
+    handleFiles(files);
+});
+
+function handleFiles(files) {
+    if (files.length > 0) {
+        const file = files[0];
+        reader.readAsDataURL(file);
+        reader.addEventListener("load", (event) => {
+            const url = event.target.result;
+            const img = document.createElement('img');
+            image.src = url;
+            dropArea.appendChild(img);
+        });
+    }
+}
